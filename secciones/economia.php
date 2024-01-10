e38c8430ae25e033e594f12d01ebe0af9bd28ee4
germanley <155853427+germanley@users.noreply.github.com>
2024-01-09 22:28:31 -0500

    Update economia.php

--- secciones/economia.php
+++ secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Silver Mountain",
+"autor" => "GESTION",
+"resumen" => "Silver Mountain realizará pagos anuales y brindará apoyo social y económico a la comunidad de Castrovirreyna (Huancavelica) para seguir realizando actividades en la mina de plata Reliquias, por 20 años más.",
 ];
 ?>
