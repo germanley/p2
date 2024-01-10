669bedcaed7ac2096e415989d5a57f409860c8e1
germanley <155853427+germanley@users.noreply.github.com>
2024-01-09 22:29:40 -0500

    Update opinion.php

--- secciones/opinion.php
+++ secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Moda 2024 ¡con absoluta libertad!",
+"autor" => "CNN",
+"resumen" => "¡Claro! No hay que dudarlo ni pensarlo mucho, pues igual que cambian tantas cosas en el mundo en 2024 nuestro estilo de vida se transforma y (afortunadamente) la moda te pide expresarte con total libertad. Adoptar tu estilo único -y ser libre de combinar tendencias-  ¡o crear las tuyas propias e inventar nuevas combinaciones!",
 ];
 ?>
