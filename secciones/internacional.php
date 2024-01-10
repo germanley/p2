b83f789818930c50632e5dd34d878082058f831f
germanley <155853427+germanley@users.noreply.github.com>
2024-01-09 22:11:24 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -1,11 +1,10 @@
 <?php
-/*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
-*****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "ECUADOR NOTICIAS",
+    "autor" => "German Leyva",
+    "resumen" => "Resumen de la Noticia",
+    "foto_url" => "https://www.google.com/search?q=ecuador+noticias&sca_esv=597084762&tbm=isch&sxsrf=ACQVn0-TM6hdI5xAIakVSyQElX4pCh7cyg:1704856195285&source=lnms&sa=X&ved=2ahUKEwic16af7NGDAxUPGLkGHTCqA-AQ_AUoAnoECAEQBA&biw=1745&bih=866&dpr=1.1#imgrc=XDObylW1w71Q8M",
 ];
+
 ?>
