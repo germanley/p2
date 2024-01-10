422c83768f871588e7ab2f27c057b4e31f4ed863
germanley <155853427+germanley@users.noreply.github.com>
2024-01-09 22:25:17 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -2,8 +2,8 @@
 
 $internacional = [
     "titulo" => "ECUADOR NOTICIAS",
-    "autor" => "German Leyva",
-    "resumen" => "Resumen de la Noticia",
+    "autor" => "El Comercio",
+    "resumen" => "Un grupo de hombres encapuchados irrumpió en las instalaciones del canal TC Televisión en Guayaquil la tarde de este martes, tras la declaración del estado de excepción por Daniel Noboa.",
     "foto_url" => "https://www.google.com/search?q=ecuador+noticias&sca_esv=597084762&tbm=isch&sxsrf=ACQVn0-TM6hdI5xAIakVSyQElX4pCh7cyg:1704856195285&source=lnms&sa=X&ved=2ahUKEwic16af7NGDAxUPGLkGHTCqA-AQ_AUoAnoECAEQBA&biw=1745&bih=866&dpr=1.1#imgrc=XDObylW1w71Q8M",
 ];
 
