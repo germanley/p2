92c97711ec1e8d77bfcfb17f678c4ef37b5b2ce1
germanley <155853427+germanley@users.noreply.github.com>
2024-01-09 22:27:25 -0500

    Update nacional.php

--- secciones/nacional.php
+++ secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Ecuador en conflicto armado",
+"autor" => "El Comercio",
+"resumen" => "La policía logró recuperar las instalaciones del canal y detuvo a treces personas, pero en otros puntos de la ciudad se produjeron hechos violentos que se saldaron con la muerte de ocho personas.",
 ];
 ?>
